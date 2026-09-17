b268f0fdf72119f6e860e11431e1fb4597b55ad3
Moises <moisesrangel97@gmail.com>
2026-09-16 23:43:29 -0300

    Adiciona exemplos de tipos de dados int, float, bool e conversão de tipos em Python

---
+++ Logica-Progamacao-Basica/aula6.py
@@ -0,0 +1,10 @@
+# conversão de tipos, coerção
+# type convertion, typecasting, coercion
+# é o ato de converte um tipo em outro
+# tipos imutáveis e primitivos:
+# str, int, float, bool
+print( 1 + 1)
+print( 'a'+'b')
+#print('1' + 1)
+print(int('1') + 1)
+print(float(1.4567) + float(2))
---
+++ Logica-Progamacao-Basica/aula7.py
@@ -0,0 +1,11 @@
+# Váriaveis são usadas para salvar algo na memória do computador
+# PEP8: inicie variáveis com letras minúsculas, pode usar
+# números e underline _.
+# O sinal de = é o operador de atribuição. Ele é usado para
+# atribuir um valor a um nome (variável).
+# Uso: nome_variavel = expressão
+
+nome_completo = "Moises Rangel"
+print(nome_completo)
+soma = 2 + 2
+print(soma)
