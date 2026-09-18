e57af89fddbf88f5977a42f3e831a4e3f54e03c9
Moises <moisesrangel97@gmail.com>
2026-09-17 23:02:38 -0300

    Adiciona exemplos de cálculo de IMC e formatação de strings em Python

---
+++ Logica-Progamacao-Basica/aula12.py
@@ -0,0 +1,5 @@
+nome = 'Moises Rangel'
+altura = 1.61
+peso = 76
+imc = peso / (altura ** 2)
+print(nome, 'tem', altura, 'pesa', peso, 'quilos e seu IMC é', imc)
---
+++ Logica-Progamacao-Basica/aula13.py
@@ -0,0 +1,9 @@
+nome = '\nMoises Rangel'
+altura = 1.61
+peso = 76
+imc = peso / (altura ** 2)
+linha_1 = f'{nome} tem {altura:.2f} de altura,'
+linha_2 = f'\npesa {peso} quilos e seu IMC é {imc:.2f}'
+print(linha_1,linha_2)
+
+# f-strings é uma formatação onde é possível atribuir strings formatadas a variaveis
